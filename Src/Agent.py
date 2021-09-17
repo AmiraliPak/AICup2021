70ee13082928f584ebfe4b2ad7c0a93241a8fb01
amirhosein <ah.pourdavoud79@gmail.com>
2021-09-17 19:06:47 +0430

    Co-authored-by: AmiraliPak <AmiraliPak@users.noreply.github.com> Co-authored-by: Fthemad <Fthemad@users.noreply.github.com>

--- Src/Agent.py
+++ Src/Agent.py
@@ -10,6 +10,7 @@ init_msg = input()
 parsed = list(map(int, init_msg.split()[1:]))
 player = Player(parsed[2], parsed[3], parsed[4], parsed[5], parsed[6], parsed[7], parsed[8])
 game_map = Map(parsed[0], parsed[1])
+maxBombRange, deadzoneStart, deadzoneDelay, maxStep = parsed[9:]
 # height, width, x, y, health, bombRange, trapCount, vision, bombDelay, maxBombRange, deadzoneStart, deadzoneDelay, maxStep = map(int, init_msg.split()[1:])
 print('init confirm')
 
@@ -31,11 +32,16 @@ while True:
         else:
             numberOfTilesInVision = state_msg_list[9]
             tileInfo = state_msg_list[9:]
+
+        game_map.update(numberOfTilesInVision, tileInfo)
         # tiles_info = []
         # counter = 0
         # for tile in range(numberOfTilesInVision):
         #     tiles_info.append((tileInfo[tile + counter], tileInfo[tile + counter + 1], tileInfo[tile + counter + 2]))
         #     counter += 2
+    
+    def next_action():
+        pass
         
     print(int(random.random() * 10))
 
